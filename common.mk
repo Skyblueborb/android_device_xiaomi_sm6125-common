c1191582b560467d1061055982760e65822505eb
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-23 16:49:09 +0200

    sm6125-common: Move to common Xiaomi light AIDL
    Change-Id: I00494cd392d19abd97cd65aeb4ec4bd9c9b11352
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- common.mk
+++ common.mk
@@ -233,7 +233,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_trinket
+    android.hardware.light-service.xiaomi
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
