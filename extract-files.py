f07b7711e0c1d1cec0c1d0160e05fce0437add4b
Skyblueborb <tomaszborbely0710@gmail.com>
2025-01-02 21:00:45 +0100

    sm6125-common: Adopt Python extract utils
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixup_remove_arch_suffix,
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_clang_rt_ubsan,
+    libs_proto_3_9_1,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+namespace_imports = [
+    "device/xiaomi/sm6125-common",
+    "hardware/qcom-caf/sm8150",
+    "hardware/qcom-caf/wlan",
+    "hardware/xiaomi",
+    "vendor/qcom/opensource/commonsys/display",
+    "vendor/qcom/opensource/commonsys-intf/display",
+    "vendor/qcom/opensource/dataservices",
+    "vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um",
+    "vendor/qcom/opensource/display",
+]
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+lib_fixups: lib_fixups_user_type = {
+    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'sm6125-common',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
