3e08817fba7f66c624275504ab608144a47a8253
Skyblueborb <tomaszborbely0710@gmail.com>
2025-01-30 17:15:52 +0100

    sm6125-common: cleanup properties and migrate to .prop files
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- common.mk
+++ common.mk
@@ -234,12 +234,6 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.vendor.qti.va_aosp.support=1
-
-PRODUCT_ODM_PROPERTIES += \
-    ro.vendor.qti.va_odm.support=1
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
