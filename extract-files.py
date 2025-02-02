9643a509c4ae8ede6000d9f32d1eb0d2bc34f8b3
Skyblueborb <tomaszborbely0710@gmail.com>
2025-02-02 19:59:03 +0100

    fix build and shim widevine

--- extract-files.py
+++ extract-files.py
@@ -49,6 +49,8 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
         .add_needed('libgui_shim.so'),
+    'vendor/lib64/libwvhidl.so': blob_fixup()
+        .add_needed('libcrypto_shim.so'),
 
 }  # fmt: skip
 
